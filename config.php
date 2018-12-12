bc494de06739bddb191574a5ecd97544306b8c25
Sasha <Sasha.mykytyuk86@ukr.net>
2018-12-12 09:33:58 +0200

    added db

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+    define('DB_NAME', 'mydb');
+define('DB_USER', 'myDbUser');
+define('DB_PASSWORD', 'Qwerty1-');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+?>
+
+
+<!--INSERT INTO `tblUsers` (`id`, `email`, `firstName`, `lastName`, `password`)
+VALUES (NULL, 'email@mail.com', 'sasha', 'mykytyuk', 'dgd4l78');-->
--- index.php
+++ index.php
@@ -1,8 +1,25 @@
+<?php require_once 'connectDB.php'?>
+
+
 <?php include_once "_header.php"; ?>
 
 
 <main role="main">
-
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">#</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+            $resault = $connect->query("SELECT id,"  )
+        ?>
+        </tbody>
+    </table>
 
 
 </main>
